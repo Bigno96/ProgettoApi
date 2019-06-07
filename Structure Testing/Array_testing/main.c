bf77e0a6883cd8567f8daa563e787c1d52e7c857
Bigno96 <andrea.bignoli96@gmail.com>
2019-06-07 22:05:49 +0200

    initialization

---
+++ Structure Testing/Array_testing/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Create dynamic Alphabetically Ordered Array for Entity Table Lookup
+ */
+int main(int argc, char** argv)
+{
+
+    return(EXIT_SUCCESS);
+}
+
